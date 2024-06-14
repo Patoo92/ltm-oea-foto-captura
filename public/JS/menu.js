1d42f8adb628860d9b6e968ba3094593fe480ac5
Pato <aplicaicon.captura@gmail.com>
2024-06-14 11:54:54 +0200

    Cambios de estilos en el menu, paso a side en vista escritorio y movil

--- public/JS/menu.js
+++ public/JS/menu.js
@@ -6,3 +6,28 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "/public/login.html";
   });
 });
+// menu.js
+document.querySelector('.toggle-btn').addEventListener('click', function() {
+  let sidebar = document.querySelector('.sidebar');
+  let content = document.querySelector('.content');
+  let toggleBtn = document.querySelector('.toggle-btn');
+  if (sidebar.style.left === '0px') {
+    sidebar.style.left = '-250px';
+    content.style.marginLeft = '0';
+    toggleBtn.style.display = 'block';
+  } else {
+    sidebar.style.left = '0';
+    content.style.marginLeft = '250px';
+    toggleBtn.style.display = 'none';
+  }
+});
+
+document.addEventListener('click', function(event) {
+  let sidebar = document.querySelector('.sidebar');
+  let toggleBtn = document.querySelector('.toggle-btn');
+  if (sidebar.style.left === '0px' && !sidebar.contains(event.target) && !toggleBtn.contains(event.target)) {
+    sidebar.style.left = '-250px';
+    document.querySelector('.content').style.marginLeft = '0';
+    toggleBtn.style.display = 'block';
+  }
+});
